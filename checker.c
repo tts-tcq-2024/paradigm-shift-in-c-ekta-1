5cef9acc72973831c4b43c1b4337948ed425341c
ekta-1 <48455720+ekta-1@users.noreply.github.com>
2024-08-07 11:32:38 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
 
-int check_range(float value, float min_value, float max_value, const char* error_message)
-{
-    if (value < min_value || value > max_value)
-    {
-        printf("%s is out of range\n", error_message);
-        return 0;
+// Function to check if battery parameters are within acceptable ranges
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    int isOk = 1; // Assume everything is ok initially  
+    if (temperature < 0 || temperature > 45) {
+        printf("Temperature out of range!\n");
+        isOk = 0;
     }
-    return 1;
-}
- 
-/*int battery_is_ok(float temperature, float soc, float charge_rate)
-{
-    return (check_range(temperature, 0, 45, "Temperature") &&
-            check_range(soc, 20, 80, "State of Charge") &&
-            check_range(charge_rate, 0, 0.8, "Charge rate"));
-}*/
+    if (soc < 20 || soc > 80) {
+        printf("State of Charge out of range!\n");
+        isOk = 0;
+    }
+    if (chargeRate > 0.8) {
+        printf("Charge Rate out of range!\n");
+        isOk = 0;
+    }
 
-int main() 
-{
-  assert(battery_is_ok(25, 70, 0.7));
-  assert(!battery_is_ok(50, 85, 0));
-  return 0;
+    return isOk;
+}
+
+int main() {
+    // Test case 1: All parameters are within acceptable ranges
+    assert(batteryIsOk(25, 70, 0.7) == 1);
+
+    // Test case 2: Temperature and SOC are out of range
+    assert(batteryIsOk(50, 85, 0) == 0);
+
+    return 0;
 }
