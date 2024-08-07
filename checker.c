1c93498a8bdbea1b92330028da0ac463b91250b8
ekta-1 <48455720+ekta-1@users.noreply.github.com>
2024-08-07 13:50:57 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,26 +3,23 @@
 
 
 // Function to check battery parameters
-   int Range_Check(float temperature, float soc, float chargeRate, const char* Error_Log)
+   int batteryIsOk(float temperature, float soc, float chargeRate)
 {
     int isTemperatureOk = (temperature >= 0 && temperature <= 45);
     int isSocOk = (soc >= 20 && soc <= 80);
     int isChargeRateOk = (chargeRate >= 0 && chargeRate <= 0.8);
-   
-    // Output error messages based on which conditions are not met
-    if ( (!isTemperatureOk) ||(!isSocOk)||(!isChargeRateOk) )
-    {
-        printf("%s is out of range\n", Error_Log);
-        return 0;
-    }
-   return 1;   
-}
 
-// Pure function to check battery status
-int batteryIsOk(float temperature, float stateOfCharge, float chargeRate) {
-    return (Range_Check(temperature,"Temperature") &&
-           Range_Check(stateOfCharge,"State of Charge") &&
-           Range_Check(chargeRate),"Charge rate");
+    // Output error messages based on which conditions are not met
+    if (!isTemperatureOk) {
+        printf("Temperature out of range! (Value: %.2f)\n", temperature);
+    }
+    if (!isSocOk) {
+        printf("State of Charge out of range! (Value: %.2f)\n", soc);
+    }
+    if (!isChargeRateOk) {
+        printf("Charge Rate out of range! (Value: %.2f)\n", chargeRate);
+    }
+    return isTemperatureOk && isSocOk && isChargeRateOk;
 }
 
 
@@ -31,5 +28,4 @@ int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
-
 
